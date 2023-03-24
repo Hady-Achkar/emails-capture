b6e43d447576256c4a713af68be45ef395dd8b3a
Hadi Alachkar <aragorn@Hadis-Laptop.local>
2023-03-24 14:15:45 +0100

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const fs = require("fs");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.post("/", (req, res) => {
+  const { name, email, phone } = req.body;
+
+  // Write the data to a file
+  fs.writeFile(
+    "data.txt",
+    `${name}, ${email}, ${phone}\n`,
+    { flag: "a" },
+    (err) => {
+      if (err) throw err;
+      console.log("Data has been written to file");
+    }
+  );
+
+  res.send(`Thanks for submitting the form, ${name}!`);
+});
+const PORT = 4040;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
